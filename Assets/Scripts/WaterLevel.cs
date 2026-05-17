4e78582e2d9e8aba1eb7a1eea4970d7ea356554d
Jacob Neilands <jn908@hotmail.com>
2026-05-17 22:16:07 +1000

    Reworked WaterLevel
    Now it should walk floodLevel up and down between 0 and 1 based on progress until the next drought or flood

--- Assets/Scripts/WaterLevel.cs
+++ Assets/Scripts/WaterLevel.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices.WindowsRuntime;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -7,13 +8,14 @@ public class WaterLevel : MonoBehaviour
     public float baseFloodTime;
     [SerializeField]
     public float baseDroughtTime;
-    public bool isGroundFlooded;
+    public bool isFlooding;
     public float timeUntilFlood;
     public float timeUntilDrought;
+    public float floodLevel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isGroundFlooded = false;
+        isFlooding = false;
         timeUntilFlood = baseFloodTime;
     }
 
@@ -21,21 +23,51 @@ public class WaterLevel : MonoBehaviour
     void Update()
     {
         //If the ground is not flooded
-        if (!isGroundFlooded)
+        if (!isFlooding)
         {
+            var quarterDone = baseFloodTime / 4;
+            var halfTime = baseFloodTime / 2;
+            var quarterLeft = quarterDone * 3;
             timeUntilFlood -= Time.deltaTime;
             if (timeUntilFlood <= 0)
             {
-                isGroundFlooded = true;
+                isFlooding = true;
                 timeUntilDrought = baseDroughtTime;
+            }else if (timeUntilFlood > quarterDone && floodLevel != 0.75f)
+            {
+                floodLevel = 0.75f;
+            } else if (timeUntilFlood > halfTime && floodLevel != 0.5f)
+            {
+                floodLevel = 0.5f;    
+            } else if (timeUntilFlood > quarterLeft && floodLevel != 0.25f)
+            {
+                floodLevel = 0.25f;
+            } else
+            {
+                floodLevel = 0;
             }
         } else
         {
+            var quarterLeft = baseDroughtTime / 4;
+            var halfTime = baseDroughtTime / 2;
+            var quarterDone = quarterLeft * 3;
             timeUntilDrought -= Time.deltaTime;
             if (timeUntilDrought <= 0)
             {
-                isGroundFlooded = false;
+                isFlooding = false;
                 timeUntilFlood = baseFloodTime;
+            }  else if (timeUntilDrought > quarterDone && floodLevel != 0.25f)
+            {
+                floodLevel = 0.25f;
+            } else if (timeUntilDrought > halfTime && floodLevel != 0.5f)
+            {
+                floodLevel = 0.5f;    
+            } else if (timeUntilDrought > quarterLeft && floodLevel != 0.75f)
+            {
+                floodLevel = 0.75f;
+            } else
+            {
+                floodLevel = 1;
             }
         }
     }
